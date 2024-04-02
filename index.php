78f7988ddce2819a12f7601986d26d5696742e40
Sanchez <selutin.alexandr90@gmail.com>
2024-04-02 19:07:30 +0300

    create blocks

--- index.php
+++ index.php
@@ -33,22 +33,32 @@
 </div>
 
 <div class="container mt-5">
-  <h3>Our articles</h3>
+  <h3 class="mb-5">Our articles</h3>
+  
+  <div class="d-flex flex-wrap">
+  <?php
+    for($i = 0; $i < 5; $i++):
+  ?>
+
   <div class="card mb-4 rounded-3 shadow-sm">
-    <div class="card-header py-3">
-      <h4 class="my-0 fw-normal">Free</h4>
-    </div>
-    <div class="card-body">
-      <h1 class="card-title pricing-card-title">$0<small class="text-body-secondary fw-light">/mo</small></h1>
-      <ul class="list-unstyled mt-3 mb-4">
-        <li>10 users included</li>
-        <li>2 GB of storage</li>
-        <li>Email support</li>
-        <li>Help center access</li>
-      </ul>
-      <button type="button" class="w-100 btn btn-lg btn-outline-primary">Sign up for free</button>
-    </div>
+      <div class="card-header py-3">
+        <h4 class="my-0 fw-normal">Free</h4>
+      </div>
+      <div class="card-body">
+        <h1 class="card-title pricing-card-title">$0<small class="text-body-secondary fw-light">/mo</small></h1>
+        <ul class="list-unstyled mt-3 mb-4">
+         <li>10 users included</li>
+          <li>2 GB of storage</li>
+          <li>Email support</li>
+          <li>Help center access</li>
+        </ul>
+        <button type="button" class="w-100 btn btn-lg btn-outline-primary">Sign up for free</button>
+      </div>
   </div>
+
+  <?php endfor; ?>
+  </div>
+
       
 </div>
   
