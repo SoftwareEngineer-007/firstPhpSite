69eb932b70c5915c8af76bb0e3347c50ea4b941e
Sanchez <selutin.alexandr90@gmail.com>
2024-04-04 17:00:11 +0300

    add blocks header and footer

---
+++ about.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+    <title>(OO=[][]=OO)</title>
+</head>
+
+<body>
+
+<?php require "blocks/header.php" ?>
+
+
+
+<?php require "blocks/footer.php" ?>
+
+</body>
+</html>
