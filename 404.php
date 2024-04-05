b0ccd3f59ef0b3cf82c2e746724042f9cb0e5d57
Sanchez <selutin.alexandr90@gmail.com>
2024-04-05 23:59:10 +0300

    404

---
+++ 404.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+    <title>(OO=[][]=OO)</title>
+</head>
+
+<body>
+
+<?php require "blocks/header.php" ?>
+
+<div class="container mt-5">
+  <h3 class="mb-5">404 ERROR</h3>
+  
+  
+  
+</div>
+
+<?php require "blocks/footer.php" ?>
+
+</body>
+</html>
